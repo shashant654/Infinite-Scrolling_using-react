b0a4993e014ed935e40fd0a9277618d1dbb5f3a2
Shashant Shekhar <shashantshekhar10@gmail.com>
2023-08-01 21:40:28 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,60 @@
+// innerHeight(1051) + scrollTop(508) = scrollheight(1559)
+
+
+import React, { useState, useEffect } from "react";
+
+import Loading from './Loading'
+import MovieComponent from "./MovieComponent";
+
+const Home = () => {
+  const [card, setCard] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+
+  const getCardData = async () => {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_limit=9&_page=${page}`
+    );
+    const data = await res.json();
+    // console.log(data);
+    setCard((prev) => [...prev, ...data]);
+    // first of all we get previous data after that we get updated new data
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getCardData();
+  }, [page]);
+
+  const handelInfiniteScroll = async () => {
+    // console.log("scrollHeight" + document.documentElement.scrollHeight);
+    // console.log("innerHeight" + window.innerHeight);
+    // console.log("scrollTop" + document.documentElement.scrollTop);
+    try {
+      if (
+        window.innerHeight + document.documentElement.scrollTop + 1 >=
+        document.documentElement.scrollHeight
+      ) {
+        setLoading(true);
+        setPage((prev) => prev + 1);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handelInfiniteScroll);
+    return () => window.removeEventListener("scroll", handelInfiniteScroll);
+  }, []);
+
+  return (
+    <>
+      <MovieComponent movieInfo={card} />
+      {loading && <Loading />} 
+    </>
+    // jbb loading will be getting true then we show <Loading />
+  );
+};
+
+export default Home;
